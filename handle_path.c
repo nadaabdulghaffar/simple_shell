45fa6ff4e246a776dd4b7963afbd0154011ee795
Nada <nadaabdulghaffar42@gmail.com>
2023-10-19 01:19:02 +0300

    3rd commit 2.1

--- handle_path.c
+++ handle_path.c
@@ -57,12 +57,12 @@ char **tokenize(char *command, ssize_t num_charin)
 
 char *search_path(char *command)
 {
-	char *path = _getenv("PATH"), *path_cpy;
 	char **path_split;
 	char *path_concat = NULL;
 	int i = 0, path_len = 0;
 	struct stat info;
-
+	char *path , *path_cpy;
+	path = _getenv("PATH");
 	if (stat(command, &info) == 0)
 		return (command);
 
@@ -70,30 +70,23 @@ char *search_path(char *command)
 
 	path_cpy = strcpy(path_cpy, path);
 	path_split = _split(path_cpy, ":");
-
 	while (path_split[i])
 	{
 		path_len = strlen(path_split[i]);
-
 		if (path_split[i][path_len - 1] != '/')
 			path_concat = strcat(path_split[i], "/");
-
 		path_concat = strcat(path_split[i], command);
 
 		if (stat(path_concat, &info) == 0)
 			break;
-
 		i++;
 	}
-
 	free(path_cpy);
-
 	if (!path_split[i])
 	{
 		free(path_split);
 		return (NULL);
 	}
-
 	free(path_split);
 	return (path_concat);
 }
